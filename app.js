4532d826c17c40aa9bf0b440f51233a6f3e041eb
thisIsBun <bun.coding@gmail.com>
2025-01-16 14:29:26 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -18,10 +19,13 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('Hello~~')
+  res.render('index')
 })
 
 app.listen(3000, () => {
-  console.log('Express server is on listening')
+  console.log('Express server is listening on http://localhost:3000/')
 })
