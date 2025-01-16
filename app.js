a8cda7e51ffd54f92aa671d51c13f74d8788edfc
thisIsBun <bun.coding@gmail.com>
2025-01-16 14:51:56 +0800

    feat: fetch data for index.js from mongodb

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const Todo = require('./models/todo')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -23,7 +24,10 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find()
+  .lean()
+  .then(todos => res.render('index', { todos }))
+  .catch(error => console.error(error))
 })
 
 app.listen(3000, () => {
