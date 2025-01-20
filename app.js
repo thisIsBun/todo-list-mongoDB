c402a0caba75a562657d70de47ca8bf57780b7ec
thisIsBun <bun.coding@gmail.com>
2025-01-20 17:54:38 +0800

    refactor: extract mongoDB connection logic into reusable file

--- app.js
+++ app.js
@@ -1,30 +1,15 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const routes = require('./routes')
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
-mongoose.connect(process.env.MONGOOSE_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,21 +1,7 @@
-const mongoose = require('mongoose')
+const db = require('../../config/mongoose')
 const Todo = require('../todo')
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-mongoose.connect(process.env.MONGOOSE_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
 db.once('open', () => {
-  console.log('mongodb connected!')
-
   for (let i = 1; i < 11; i++) {
     Todo.create({ name: `todo-${i}`})
   }
