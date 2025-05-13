ce74dcabf6c3895d77c129386f0893a025170d95
thisIsBun <bun.coding@gmail.com>
2025-05-13 18:07:16 +0800

    feat: add users route handling module

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import home from './modules/home.js'
 import todos from './modules/todos.js'
+import users from './modules/users.js'
 
 const router = express.Router();
 router.use('/', home);
 router.use('/todos', todos);
+router.use('/users', users);
 
 export default router;
