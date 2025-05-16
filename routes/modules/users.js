5f383dd317b7611368ea09503ae2c14032fdf9e7
thisIsBun <bun.coding@gmail.com>
2025-05-16 16:46:44 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -6,4 +6,11 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
+router.post('/login', (req, res) => {
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
 export default router;
