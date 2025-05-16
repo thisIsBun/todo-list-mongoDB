cc0538273283f5e9252c70884901502ae3a1b010
thisIsBun <bun.coding@gmail.com>
2025-05-16 17:39:13 +0800

    feat: add user model

---
+++ models/user.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+const { Schema } = mongoose
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+export default mongoose.model('User', userSchema)
