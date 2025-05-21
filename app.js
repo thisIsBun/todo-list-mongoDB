f9930f2ac6600d7de4a98eec8639f9cf5ba1af94
thisIsBun <bun.coding@gmail.com>
2025-05-21 16:22:14 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ import { engine } from 'express-handlebars';
 import methodOverride from './config/methodOverride.cjs';
 import router from './routes/index.js'
 import './config/mongoose.js'
+import session from 'express-session'
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', 'hbs');
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(router);
