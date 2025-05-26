8eb5eb21a09124be67d738ccf0a241d4da93a208
thisIsBun <bun.coding@gmail.com>
2025-05-26 17:47:55 +0800

    feat: add bcrypy for user register

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import User from '../../models/user.js';
 import passport from 'passport';
+import bcryptjs from 'bcryptjs';
 
 const router = express.Router();
 
@@ -34,19 +35,17 @@ router.post('/register', (req, res) => {
   }
 
   User.findOne({ email })
-    .then((user) => {
-      if (user) {
-        errors.push({ message: 'Email 已經註冊過了。'})
-        return res.render('register', { errors, name, email, password, confirmPassword });
-      } 
-      
-      User.create({ name, email, password })
-        .then(() => {
-          res.redirect('/');
-        })
-        .catch((error) => console.log(error));
-    })
-    .catch((error) => console.log(error));
+  .then(user => {
+    if (user) {
+      errors.push({ message: 'Email 已經註冊過了。'})
+      return res.render('register', { errors, name, email, password, confirmPassword });
+    }      
+    return bcryptjs.genSalt(10)
+  })
+  .then(salt => bcryptjs.hash(password, salt))
+  .then(hash => User.create({ name, email, password: hash }))
+  .then(() => res.redirect('/'))
+  .catch((error) => console.log(error));
 });
 
 router.get('/logout', (req, res) => {
